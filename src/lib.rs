ad6af51abd5e6bce83773115d1b74e99fb7b600c
Antoine Poinsot <darosior@protonmail.com>
2021-03-09 14:56:26 +0100

    Document modules, remove the 'error' module
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
+/// The logic to parse our static config (Noise keys, managers keys, ..)
 pub mod config;
+
+/// The logic to initiate our main state
 pub mod cosignerd;
+
+/// The database query and update logic
 pub mod database;
+
+/// Protocol message processing, we only have to handle a single message.
 pub mod processing;
+
+/// FIXME: does not make sense to have an 'util' module
 pub mod utils;

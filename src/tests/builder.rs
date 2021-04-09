44d5b82b2b2ef17ba5d6161cbb1fca806c55f1cf
Antoine Poinsot <darosior@protonmail.com>
2021-04-09 10:38:30 +0200

    Cargo: update to latest revault_tx
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

--- src/tests/builder.rs
+++ src/tests/builder.rs
@@ -5,14 +5,12 @@ use revault_tx::{
         bitcoin::{
             secp256k1,
             secp256k1::rand::{rngs::SmallRng, FromEntropy, RngCore},
-            util::bip32::{self, ChildNumber},
+            util::bip32,
             Network, OutPoint, TxOut,
         },
-        descriptor::{
-            DescriptorPublicKey, DescriptorPublicKeyCtx, DescriptorSinglePub, DescriptorXKey,
-        },
+        descriptor::{DescriptorPublicKey, DescriptorSinglePub, DescriptorXKey, Wildcard},
     },
-    scripts::{cpfp_descriptor, unvault_descriptor},
+    scripts::{CpfpDescriptor, UnvaultDescriptor},
     transactions::SpendTransaction,
     txins::UnvaultTxIn,
     txouts::{ExternalTxOut, SpendTxOut, UnvaultTxOut},
@@ -51,7 +49,7 @@ impl CosignerTestBuilder {
                 origin: None,
                 xkey: bip32::ExtendedPubKey::from_private(&secp, &random_privkey(&mut rng)),
                 derivation_path: bip32::DerivationPath::from(vec![]),
-                is_wildcard: true,
+                wildcard: Wildcard::Unhardened,
             });
             managers_keys.push(xpub);
 
@@ -102,7 +100,6 @@ impl CosignerTestBuilder {
     pub fn generate_spend_tx(&self, outpoints: &[OutPoint]) -> SpendTransaction {
         let mut rng = SmallRng::from_entropy();
         let secp = secp256k1::Secp256k1::new();
-        let xpub_ctx = DescriptorPublicKeyCtx::new(&secp, ChildNumber::from(0));
         let unvault_value: u64 = 100000000;
         let n_stk = 10;
         let csv = 12;
@@ -114,7 +111,7 @@ impl CosignerTestBuilder {
                 origin: None,
                 xkey: bip32::ExtendedPubKey::from_private(&secp, &random_privkey(&mut rng)),
                 derivation_path: bip32::DerivationPath::from(vec![]),
-                is_wildcard: true,
+                wildcard: Wildcard::Unhardened,
             }));
             cosigners_keys.push(DescriptorPublicKey::SinglePub(DescriptorSinglePub {
                 origin: None,
@@ -122,7 +119,7 @@ impl CosignerTestBuilder {
                     .public_key,
             }));
         }
-        let unvault_descriptor = unvault_descriptor(
+        let unvault_descriptor = UnvaultDescriptor::new(
             stakeholders_keys,
             self.managers_keys.clone(),
             1,
@@ -131,12 +128,13 @@ impl CosignerTestBuilder {
         )
         .expect("Unvault descriptor generation error");
         let cpfp_descriptor =
-            cpfp_descriptor(self.managers_keys.clone()).expect("CPFP desc generation error");
+            CpfpDescriptor::new(self.managers_keys.clone()).expect("CPFP desc generation error");
 
         let unvault_txins: Vec<UnvaultTxIn> = outpoints
             .iter()
             .map(|o| {
-                let unvault_txout = UnvaultTxOut::new(unvault_value, &unvault_descriptor, xpub_ctx);
+                let unvault_txout =
+                    UnvaultTxOut::new(unvault_value, &unvault_descriptor.derive(0.into(), &secp));
                 UnvaultTxIn::new(*o, unvault_txout, csv)
             })
             .collect();
@@ -148,8 +146,7 @@ impl CosignerTestBuilder {
         SpendTransaction::new(
             unvault_txins,
             vec![SpendTxOut::Destination(spend_txo.clone())],
-            &cpfp_descriptor,
-            xpub_ctx,
+            &cpfp_descriptor.derive(0.into(), &secp),
             0,
             true,
         )
